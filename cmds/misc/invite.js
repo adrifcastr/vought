0e98fe07a800a38df3c95bdbb2c82cc111b42a7e
adrifcastr <adrifcastr@gmail.com>
2020-10-15 19:55:15 +0200

    add invite.js

---
+++ cmds/misc/invite.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Discord.Message} message
+ */
+export async function run(message) {     
+    message.channel.send('https://discord.gg/m55BG9d'); 
+}
+
+export const help = {
+    name: 'invite',
+    type: 'misc',
+    help_text: 'invite',
+    help_desc: 'Sends a permanent invite link',
+    owner: false,
+    voice: false,
+    timevault: false,
+    nsfw: false,
+    args: {},
+    roles: [],
+    user_perms: [],
+    bot_perms: []
+};
