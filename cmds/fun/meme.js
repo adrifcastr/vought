1ed8ff06b3cab4c23b15b9fcfbe817d31ae88d0d
adrifcastr <adrifcastr@gmail.com>
2020-10-16 10:43:52 +0200

    actually commit moved files

---
+++ cmds/fun/meme.js
@@ -0,0 +1,20 @@
+import Util from '../../Util.js';
+
+/**
+ * @param {Discord.Message} message
+ */
+export async function run(message) {
+   Util.IMG('uellKvP', message);
+}
+
+export const help = {
+    name: 'meme',
+    type: 'fun',
+    help_text: 'meme',
+    help_desc: 'Fetches a random The Boys meme',
+    owner: false,
+    args: {},
+    roles: [],
+    user_perms: [],
+    bot_perms: []
+};
