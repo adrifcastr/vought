02c23ca3cded72ce00125bf4b4d584a9c6114e5c
adrifcastr <adrifcastr@gmail.com>
2020-10-17 19:16:19 +0200

    add quote.js

---
+++ cmds/fun/quote.js
@@ -0,0 +1,27 @@
+import Util from '../../Util.js';
+
+/**
+ * @param {Discord.Message} message
+ */
+export async function run(message) {
+   const body = await Util.fetchJSON('https://gideonbot.com/api/quotes/theboys').catch(ex => Util.log(ex));
+   const quote = body[Math.floor(Math.random() * body.length)];
+
+   const embed = Util.Embed()
+   .setDescription('**' + quote.quote + '\n\n~' + quote.author + '**')
+   .setThumbnail(quote.img)
+
+   message.channel.send(embed);
+}
+
+export const help = {
+    name: 'quote',
+    type: 'fun',
+    help_text: 'meme',
+    help_desc: 'Fetches a random The Boys quote',
+    owner: false,
+    args: {},
+    roles: [],
+    user_perms: [],
+    bot_perms: []
+};
