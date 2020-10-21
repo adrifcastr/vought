a5235d94edc97ff12cd5e6e32aac8f5c2ca7c0da
adrifcastr <adrifcastr@gmail.com>
2020-10-21 21:37:05 +0200

    changes

--- install_node_13.sh
+++
@@ -1,6 +0,0 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="/app/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm --version
-nvm install node
-nvm use node
