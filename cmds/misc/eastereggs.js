39bfaed3a1a828d1212f36b638379aba69342763
adrifcastr <adrifcastr@gmail.com>
2020-10-21 20:45:56 +0200

    add eggs

---
+++ cmds/misc/eastereggs.js
@@ -0,0 +1,20 @@
+import Util from '../../Util.js';
+
+/**
+ * @param {Discord.Message} message
+ */
+export async function run(message) {
+    message.channel.send(Util.Embed().setTitle('Available eastereggs:').setDescription('`#hughlight`\n`wish + by + starlight`\n`deep + soy + sauce`\n`brave + maeve + pride + bar`\n`boom + pow + noir`\n`everyone + get + the + fuck + out`' + process.logos));
+}
+
+export const help = {
+    name: ['eggs'],
+    type: 'misc',
+    help_text: 'eggs',
+    help_desc: 'List all eastereggs',
+    owner: false,
+    args: {},
+    roles: [],
+    user_perms: [],
+    bot_perms: []
+};
