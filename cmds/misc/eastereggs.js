9c6feab2c94c514c34549f1fc4de8e116fe3a90a
adrifcastr <adrifcastr@gmail.com>
2020-10-24 13:28:36 +0200

    more greatness

--- cmds/misc/eastereggs.js
+++ cmds/misc/eastereggs.js
@@ -4,7 +4,7 @@ import Util from '../../Util.js';
  * @param {Discord.Message} message
  */
 export async function run(message) {
-    message.channel.send(Util.Embed().setTitle('Available eastereggs:').setDescription('`#hughlight`\n`wish + by + starlight`\n`deep + soy + sauce`\n`brave + maeve + pride + bar`\n`boom + pow + noir`\n`everyone + get + the + fuck + out`\n`begone + thot`\n`starlight + black + noir + floss`\n`1-555-BE-SUPER`\n`1-555-SUPEHAUS`\n`supeporn.com`\n`translucent + tribute + invisible + coin`\n`voughtlife + classics + power + ballads`\n`weird + with + milk + dangerous + to + tits`\n`you\'ll + never + truly + vanish`' + process.logos));
+    message.channel.send(Util.Embed().setTitle('Available eastereggs:').setDescription('`#hughlight`\n`wish + by + starlight`\n`deep + soy + sauce`\n`brave + maeve + pride + bar`\n`boom + pow + noir`\n`everyone + get + the + fuck + out`\n`begone + thot`\n`starlight + black + noir + floss`\n`1-555-BE-SUPER`\n`1-555-SUPEHAUS`\n`supeporn.com`\n`translucent + tribute + invisible + coin`\n`voughtlife + classics + power + ballads`\n`weird + with + milk + dangerous + to + tits`\n`you\'ll + never + truly + vanish`\n`i + can + do + whatever + the + fuck + i + want`' + process.logos));
 }
 
 export const help = {
