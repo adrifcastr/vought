b8421e34cda17626300a4eb73d123552acce5c6f
adrifcastr <adrifcastr@gmail.com>
2020-11-01 12:14:26 +0100

    checkity check yoself

--- cmds/admin/restrict.js
+++ cmds/admin/restrict.js
@@ -9,6 +9,7 @@ export async function run(message, args) {
     if (message.mentions.members.first()) member = message.mentions.members.first();
     else if (Util.ValID(args[0])) member = await message.guild.members.fetch(args[0]).catch(ex => Util.log(ex));
     
+    if (member.guild.id !== '604160368490577930') return message.reply('not available in this guild!');
     if (!member.manageable) return message.reply('I cannot restrict this member due to either it being the guild owner or having a higher role then me.');
 
     if (member.roles.cache.has('768904238629388328')) {
